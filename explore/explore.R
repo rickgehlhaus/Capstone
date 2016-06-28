ac59747582441a622cf0092ea6e14ef8a32644e0
Rick Gehlhaus <rickgehlhaus@gmail.com>
2016-06-27 21:05:25 -0700

    Update explore.R

--- explore/explore.R
+++ explore/explore.R
@@ -1,5 +1,11 @@
 library(data.table)
 library(bit64)
+library(dplyr)
+library(ggplot2)
 set.seed(11)
-tran <- fread("transactions.csv", nrows = 5000)
+tran <- fread("transactions.csv", nrows = 100000)
 str(tran)
+p <- ggplot(tran, aes(x = purchasequantity, y = purchaseamount)) +
+  geom_point() +
+  ggtitle("explore")
+p + theme_economist() + scale_colour_economist()
